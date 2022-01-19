09fe6ed47682d1fb76e38808cdb79ef3ebb830a0
Brenon Parry <brenonparry@gmail.com>
2022-01-19 15:32:35 -0800

    use props to create dynamic data and styles for component

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import "components/InterviewerList.scss"
+import InterviewerListItem from './InterviewerListItem';
+import classNames from 'classnames';
 
-export default function InterviewerList() {
+export default function InterviewerList(props) {
+
+  const interviewerList = props.interviewers.map(interviewer => <InterviewerListItem 
+    key={interviewer.id} 
+    {...interviewer} 
+    selected={interviewer.id === props.interviewer} 
+    setInterviewer={props.setInterviewer} 
+    />);
+
+    const listClass = classNames("interviewers__list", {
+      "interviewers__list--selected": props.selected
+    })
+
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className={listClass} onClick={() => props.setInterviewer}>{ interviewerList }</ul>
     </section>
   )
 }
