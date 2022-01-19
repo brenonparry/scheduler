d0e9fa27319ee2725f3d32274c459ff11636e9b6
Brenon Parry <brenonparry@gmail.com>
2022-01-18 18:01:54 -0800

    map over days array to return values to be used in dynamic creation og <DaylistItem/>

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,30 +2,10 @@ import React from 'react'
 import DayListItem from './DayListItem'
 
 export default function DayList(props) {
-  
+  const daylist = props.days.map(day => <DayListItem key={day.id} {...day} selected={day.name === props.day} setDay={props.setDay} />)
   return (
     <ul>
-    <DayListItem 
-      key={props.days[0].id}
-      name={props.days[0].name} 
-      spots={props.days[0].spots} 
-      selected={props.days[0].name === props.day}
-      setDay={props.setDay}  
-    />
-    <DayListItem
-      key={props.days[1].id} 
-      name={props.days[1].name} 
-      spots={props.days[1].spots} 
-      selected={props.days[1].name === props.day}
-      setDay={props.setDay}  
-    />
-    <DayListItem 
-      key={props.days[2].id}
-      name={props.days[2].name}
-      spots={props.days[2].spots} 
-      selected={props.days[2].name === props.day}
-      setDay={props.setDay}  
-    />      
-  </ul>
+     {daylist}
+   </ul>
   )
 }
