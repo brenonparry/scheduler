4fc4ba16cb661619c530dec4b983ef8b3af2654d
Brenon Parry <brenonparry@gmail.com>
2022-01-19 13:58:18 -0800

    add dynamic className and data to component using props

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import "components/InterviewerListItem.scss"
+import classNames from 'classnames'
 
-export default function InterviewerListItem() {
+export default function InterviewerListItem(props) {
+
+  const listClass = classNames("interviewers__item", {
+      "interviewers__item--selected": props.selected,
+    }
+  )
+
   return (
-  <li className="interviewers__item">
+  <li className={listClass} onClick={() => props.setInterviewer(props.id)}>
     <img
       className="interviewers__item-image"
-      src="https://i.imgur.com/LpaY82x.png"
+      src={ props.avatar }
       alt="Sylvia Palmer"
     />
-    Sylvia Palmer
+    {props.selected === true && <>{ props.name }</>}
   </li>
   )
 }
