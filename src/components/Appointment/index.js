60401bb50164ce9de6b611dc15519849804ebbfe
Brenon Parry <brenonparry@gmail.com>
2022-01-19 23:13:38 -0800

    Add mock data to Application and add Appointment component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,16 +1,25 @@
 import React from 'react'
 import "components/Appointment/styles.scss"
+import Header from 'components/Appointment/Header';
+import Show from 'components/Appointment/Show';
+import Empty from 'components/Appointment/Empty';
+
 
 export default function Appointment(props) {
 
-  const appointmentTime = () => {
-    let text = "No Appointments"
-    if (props.time) {
-      text = "Appointment at " + (props.time);
-    }
-    return text
-  }
+  // const appointmentTime = () => {
+  //   let text = "No Appointments"
+  //   if (props.time) {
+  //     text = "Appointment at " + (props.time);
+  //   }
+  //   return text
+  // }
   return (
-    <article className="appointment">{appointmentTime()}</article>
+    <article className="appointment">
+    <Header time={props.time}/>
+    {props.interview ? <Show key={props.id} student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty />}
+    </article>
+
+  
   )
 }
