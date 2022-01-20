cbf09dea70d6b5962f3a72f7ac77323990c4a597
Brenon Parry <brenonparry@gmail.com>
2022-01-19 19:40:01 -0800

    add Confirm story to master Appoinment stories with props

--- stories/index.js
+++ stories/index.js
@@ -15,6 +15,7 @@ import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
 
 storiesOf("Button", module)
   .addParameters({
@@ -162,3 +163,10 @@ storiesOf("Appointment", module)
       onEdit={action("onEdit")}
       onDelete={action("onDelete")}
     />))
+
+    .add("Confirm", () => (
+      <Confirm
+        messege="Delete the appointment"
+        onConfirm={action("onConfirm")}
+        onCancel={action("onCancel")}
+      />))
