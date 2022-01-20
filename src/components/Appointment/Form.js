77399ca379c306b794f5f59e63fa00d006d5098f
Brenon Parry <brenonparry@gmail.com>
2022-01-19 21:59:32 -0800

    update code to use props passed from stories

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,33 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from 'components/Button'
 import InterviewerList from 'components/InterviewerList'
 
 
 export default function Form(props) {
 
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  
+  const reset = () => {
+    setInterviewer(null);
+    setStudent('');
+  };
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  };
+
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form onSubmit={event => event.preventDefault()} autoComplete="off">
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
+            onChange={(event) => setStudent(event.target.value)}
             placeholder="Enter Student Name"
-          /*
-            This must be a controlled component
-            your code goes here
-          */
+            value={student}
           />
         </form>
         <InterviewerList
-        /* your code goes here */
+           interviewers={props.interviewers}
+           value={interviewer}
+           onChange={setInterviewer}
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger>Cancel</Button>
-          <Button confirm>Save</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onClick={props.onSave}>Save</Button>
         </section>
       </section>
     </main>
