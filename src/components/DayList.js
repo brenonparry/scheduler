9cb6d0f5fd7f9d476b95e0630355cda7842bf493
Brenon Parry <brenonparry@gmail.com>
2022-01-20 10:06:10 -0800

    bug fix: found misnamed props and fixed so seleceted days styling is applied

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,7 +2,7 @@ import React from 'react'
 import DayListItem from './DayListItem'
 
 export default function DayList(props) {
-  const daylist = props.days.map(day => <DayListItem key={day.id} {...day} selected={props.name === props.value} setDay={props.onChange} />)
+  const daylist = props.days.map(day => <DayListItem key={day.id} {...day} selected={day.name === props.value} setDay={props.setDay} />)
   return (
     <ul>
      {daylist}
