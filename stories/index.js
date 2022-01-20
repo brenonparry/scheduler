00590cf3847f1bcde550e8beb59ebc7423a95a54
Brenon Parry <brenonparry@gmail.com>
2022-01-19 19:47:55 -0800

    add Status story to master Appoinment stories with props

--- stories/index.js
+++ stories/index.js
@@ -16,6 +16,7 @@ import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
 
 storiesOf("Button", module)
   .addParameters({
@@ -164,9 +165,12 @@ storiesOf("Appointment", module)
       onDelete={action("onDelete")}
     />))
 
-    .add("Confirm", () => (
-      <Confirm
-        messege="Delete the appointment"
-        onConfirm={action("onConfirm")}
-        onCancel={action("onCancel")}
-      />))
+  .add("Confirm", () => (
+    <Confirm
+      messege="Delete the appointment"
+      onConfirm={action("onConfirm")}
+      onCancel={action("onCancel")}
+    />))
+  
+  .add("Status", () => <Status message="Deleting"/>)
+    
