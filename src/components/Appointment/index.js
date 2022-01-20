02d5168101643265fd0b415fc6cebb2d49cade13
Brenon Parry <brenonparry@gmail.com>
2022-01-19 18:48:46 -0800

    Add placeholder code for stories to check

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,7 +2,15 @@ import React from 'react'
 import "components/Appointment/styles.scss"
 
 export default function Appointment(props) {
+
+  const appointmentTime = () => {
+    let text = "No Appointments"
+    if (props.time) {
+      text = "Appointment at " + (props.time);
+    }
+    return text
+  }
   return (
-    <article className="appointment"></article>
+    <article className="appointment">{appointmentTime()}</article>
   )
 }
