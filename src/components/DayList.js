f0ff459079303b3716c59e8505e1abfb823a6c42
Brenon Parry <brenonparry@gmail.com>
2022-01-19 16:42:19 -0800

    update the props being passed to DaylistItem from Daylist to follow a controlled list naming pattern

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,7 +2,7 @@ import React from 'react'
 import DayListItem from './DayListItem'
 
 export default function DayList(props) {
-  const daylist = props.days.map(day => <DayListItem key={day.id} {...day} selected={day.name === props.day} setDay={props.setDay} />)
+  const daylist = props.days.map(day => <DayListItem key={day.id} {...day} selected={props.name === props.value} setDay={props.onChange} />)
   return (
     <ul>
      {daylist}
