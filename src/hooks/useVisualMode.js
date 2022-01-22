caaaf0e14cb849331bab60ad52f30a13b494aa7e
Brenon Parry <brenonparry@gmail.com>
2022-01-22 11:10:55 -0800

    used visual-mode repo to craft transition and back functions for custom hook.

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,7 +3,25 @@ import { useState } from 'react';
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  return { mode };
+  const transition = (newMode, replace = false) => {
+
+    if (!replace) {
+      setHistory([...history, newMode]);
+    }
+    setMode(newMode);
+  };
+   
+  const back = () => {
+    
+    if (history.length > 1) {
+      history.pop();
+    }
+    const previous = history.slice(-1)[0];
+    setMode(previous);
+  };
+
+  return { mode, transition, back, history };
+
 }
-
