3bb923d221beb438f0be671d5b477635970f38d6
Brenon Parry <brenonparry@gmail.com>
2022-01-26 10:23:27 -0800

    add validate() function to Form and impliment it in onClick event when saving to check if student input field is blank

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -8,7 +8,8 @@ export default function Form(props) {
 
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
-  
+  const [error, setError] = useState("");
+
   const reset = () => {
     setInterviewer(null);
     setStudent('');
@@ -18,6 +19,14 @@ export default function Form(props) {
     props.onCancel();
   };
 
+  const validate = () => {
+    if (student === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+  
+    props.onSave(student, interviewer);
+  }
 
   return (
     <main className="appointment__card appointment__card--create">
@@ -32,6 +41,7 @@ export default function Form(props) {
             value={student}
             data-testid="student-name-input"
           />
+        <section className="appointment__validation">{error}</section>
         </form>
         <InterviewerList
            interviewers={props.interviewers}
@@ -42,7 +52,7 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={() => props.onSave(student, interviewer)}>Save</Button>
+          <Button confirm onClick={validate}>Save</Button>
         </section>
       </section>
     </main>
