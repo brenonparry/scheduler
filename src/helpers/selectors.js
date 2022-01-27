e5c5a0cb43a91ddb569f5a42dd72d915a7d44bd6
Brenon Parry <brenonparry@gmail.com>
2022-01-26 20:18:39 -0800

    run format and add missing semi colons

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -6,7 +6,7 @@ export const getAppointmentsForDay = (state, day) => {
   if (filteredDays.length !== 0) {
     for (const element of filteredDays[0].appointments) {
       appointmentArray.push(state.appointments[element]);
-    
+
     };
   };
 
@@ -15,10 +15,10 @@ export const getAppointmentsForDay = (state, day) => {
 
 export const getInterview = (state, interveiw) => {
 
-  if(interveiw) {
-    return {...interveiw, interviewer: state.interviewers[interveiw.interviewer]};
+  if (interveiw) {
+    return { ...interveiw, interviewer: state.interviewers[interveiw.interviewer] };
   };
-  
+
   return null;
 };
 
@@ -26,11 +26,11 @@ export const getInterviewersForDay = (state, day) => {
   const filteredDays = state.days.filter((filteredDay) => filteredDay.name === day);
 
   let interviewersArray = [];
-  
+
   if (filteredDays[0]) {
     const interviewersById = filteredDays[0].interviewers
     interviewersById.map((interviewer) => interviewersArray.push(state.interviewers[interviewer]));
-  }
+  };
 
   return interviewersArray;
 };
