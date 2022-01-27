65cd348ee670d8994ed15edd450d41d4fe7d2bc2
Brenon Parry <brenonparry@gmail.com>
2022-01-26 20:16:20 -0800

    run format and add missing semi colons

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -6,27 +6,27 @@ import PropTypes from 'prop-types';
 
 
 export default function InterviewerList(props) {
-  
+
   InterviewerList.propTypes = {
     interviewers: PropTypes.array.isRequired
   };
 
-  const interviewerList = props.interviewers.map(interviewer => <InterviewerListItem 
+  const interviewerList = props.interviewers.map(interviewer => <InterviewerListItem
     key={interviewer.id}
     name={interviewer.name}
     avatar={interviewer.avatar}
     selected={interviewer.id === props.value}
-    setInterviewer={() => props.onChange(interviewer.id)}  
-    />);
+    setInterviewer={() => props.onChange(interviewer.id)}
+  />);
 
-    const listClass = classNames("interviewers__list", {
-      "interviewers__list--selected": props.selected
-    })
+  const listClass = classNames("interviewers__list", {
+    "interviewers__list--selected": props.selected
+  });
 
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className={listClass} onClick={props.setInterviewer}>{ interviewerList }</ul>
+      <ul className={listClass} onClick={props.setInterviewer}>{interviewerList}</ul>
     </section>
-  )
-}
+  );
+};
--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -5,18 +5,18 @@ import classNames from 'classnames'
 export default function InterviewerListItem(props) {
 
   const listClass = classNames("interviewers__item", {
-      "interviewers__item--selected": props.selected,
-    }
-  )
+    "interviewers__item--selected": props.selected,
+  }
+  );
 
   return (
-  <li className={listClass} onClick={props.setInterviewer}>
-    <img
-      className="interviewers__item-image"
-      src={ props.avatar }
-      alt={ props.name }
-    />
-    {props.selected && props.name }
-  </li>
-  )
-}
+    <li className={listClass} onClick={props.setInterviewer}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name}
+    </li>
+  );
+};
